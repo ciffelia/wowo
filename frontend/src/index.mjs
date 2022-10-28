b9085bfd27877c405a9a63bd2f71f857c4c5755c
Ciffelia <mc.prince.0203@gmail.com>
2022-10-28 14:17:59 +0900

    Refactor index.mjs

--- frontend/src/index.mjs
+++ frontend/src/index.mjs
@@ -1,42 +1,45 @@
-(() => {
-  const wakeButton = document.querySelector('.wake-button');
-  const message = document.querySelector('.message');
-  const messageHeader = document.querySelector('.message-header');
-  const messageBody = document.querySelector('.message-body');
+const wakeButton = document.querySelector('.wake-button');
+const message = document.querySelector('.message');
+const messageHeader = document.querySelector('.message-header');
+const messageBody = document.querySelector('.message-body');
 
-  const showMessage = (msgType, title, content) => {
-    message.classList.add(`is-${msgType}`);
-    messageHeader.innerText = title;
-    messageBody.innerText = content;
+wakeButton.addEventListener('click', async () => {
+  wakeButton.classList.add('is-loading');
 
-    message.classList.remove('is-hidden');
-  };
-
-  wakeButton.addEventListener('click', (evt) => {
-    wakeButton.classList.add('is-loading');
-
-    fetch('./api/wake', {
+  try {
+    const resp = await fetch('./api/wake', {
       method: 'POST',
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then((response) => {
-        const text = `Command exited with code ${response.exitCode}:\n${response.output}`;
-        if (response.exitCode === 0) {
-          showMessage('success', 'Success', text);
-        } else {
-          showMessage('danger', 'Error', text);
-        }
-      })
-      .catch((err) => {
-        showMessage('danger', 'Error', err.toString());
-      })
-      .finally(() => {
-        wakeButton.classList.add('is-hidden');
-      });
-  });
-})();
+    });
+    if (!resp.ok) {
+      throw new Error(`HTTP ${resp.status} ${resp.statusText}`);
+    }
+
+    const result = await resp.json();
+    const text = `Command exited with code ${result.exitCode}:\n${result.output}`;
+    if (result.exitCode === 0) {
+      showSuccessMessage(text);
+    } else {
+      showErrorMessage(text);
+    }
+  } catch (err) {
+    showErrorMessage(err.toString());
+  } finally {
+    wakeButton.classList.add('is-hidden');
+  }
+});
+
+const showSuccessMessage = (content) => {
+  showMessage('success', 'Success', content);
+};
+
+const showErrorMessage = (content) => {
+  showMessage('danger', 'Error', content);
+};
+
+const showMessage = (msgType, title, content) => {
+  message.classList.add(`is-${msgType}`);
+  messageHeader.innerText = title;
+  messageBody.innerText = content;
+
+  message.classList.remove('is-hidden');
+};
