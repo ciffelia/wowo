bbc3b17fddc2aec331780764a878cd0a4f9b5584
Ciffelia <mc.prince.0203@gmail.com>
2022-10-28 13:57:35 +0900

    Run `prettier --write .`

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,42 +1,42 @@
 (() => {
-  const wakeButton = document.querySelector('.wake-button')
-  const message = document.querySelector('.message')
-  const messageHeader = document.querySelector('.message-header')
-  const messageBody = document.querySelector('.message-body')
+  const wakeButton = document.querySelector('.wake-button');
+  const message = document.querySelector('.message');
+  const messageHeader = document.querySelector('.message-header');
+  const messageBody = document.querySelector('.message-body');
 
   const showMessage = (msgType, title, content) => {
-    message.classList.add(`is-${msgType}`)
-    messageHeader.innerText = title
-    messageBody.innerText = content
+    message.classList.add(`is-${msgType}`);
+    messageHeader.innerText = title;
+    messageBody.innerText = content;
 
-    message.classList.remove('is-hidden')
-  }
+    message.classList.remove('is-hidden');
+  };
 
-  wakeButton.addEventListener('click', evt => {
-    wakeButton.classList.add('is-loading')
+  wakeButton.addEventListener('click', (evt) => {
+    wakeButton.classList.add('is-loading');
 
     fetch('./api/wake', {
-      method: 'POST'
+      method: 'POST',
     })
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
-          throw new Error(`HTTP ${response.status} ${response.statusText}`)
+          throw new Error(`HTTP ${response.status} ${response.statusText}`);
         }
-        return response.json()
+        return response.json();
       })
-      .then(response => {
-        const text = `Command exited with code ${response.exitCode}:\n${response.output}`
+      .then((response) => {
+        const text = `Command exited with code ${response.exitCode}:\n${response.output}`;
         if (response.exitCode === 0) {
-          showMessage('success', 'Success', text)
+          showMessage('success', 'Success', text);
         } else {
-          showMessage('danger', 'Error', text)
+          showMessage('danger', 'Error', text);
         }
       })
-      .catch(err => {
-        showMessage('danger', 'Error', err.toString())
+      .catch((err) => {
+        showMessage('danger', 'Error', err.toString());
       })
       .finally(() => {
-        wakeButton.classList.add('is-hidden')
-      })
-  })
-})()
+        wakeButton.classList.add('is-hidden');
+      });
+  });
+})();
